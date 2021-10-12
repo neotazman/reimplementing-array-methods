89ced6807d96450692e3fc686dccc845b89fac3c
Tristan Alie <TristanAlie@yahoo.com>
2021-10-12 15:23:54 -0400

    completed newJoin

--- code.js
+++ code.js
@@ -23,3 +23,33 @@ const newConcat = (array1, array2) => { //loops through both arrays and adds the
     return newArray
 }
 
+const newJoin = (array, separator) => { //turns the array into a string separated by the separator
+    if(typeof separator !== 'string') { //makes sure the separator is a string
+        return null
+    }
+
+    let newArray = []
+    let newString = ''
+
+    for (let i = 0; i < array.length; i++) { //stringifies each value
+        if(typeof array[i] === 'string') { //stringifying a string adds quotation marks, so i just push the string as is
+            newArray.push(array[i])
+        } else {
+            newArray.push(JSON.stringify(array[i]))
+        }
+        
+    }
+
+    if(newArray.length === 1) { //if the array has exactly one value, it just returns the one value without the separator
+        newString += newArray[0]
+        return newString
+    } else { //loops through each value of the newArray and concatenates it with the separator
+        for(let i = 0; i < newArray.length; i++) {
+            newString += newArray[i]
+            if(i !== newArray.length - 1) { //it doesn't add the separator if it's the last value
+                newString += separator
+            }
+        }
+    }
+    return newString
+}
