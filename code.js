f0ccf1032de8ec5f49e13602c1f8578ceb483990
Tristan Alie <TristanAlie@yahoo.com>
2021-10-12 15:00:47 -0400

    completed newincludes and newconcat

--- code.js
+++ code.js
@@ -1,1 +1,25 @@
 // Your Code Here.
+
+const dummyArray2 = [6, 4, 8]
+const dummyArray = [1, 4, -6, 'a', 'jello', 24, false];
+
+const newIncludes = (array, value) => { //loops through the array to find the value
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] === value) {
+            return true;
+        }
+    }
+    return false; //if it goes through the entire array, and doesn't return true, it returns false
+}
+
+const newConcat = (array1, array2) => { //loops through both arrays and adds them to a new array
+    let newArray = []
+    for (let i = 0; i < array1.length; i++) {
+        newArray.push(array1[i])
+    }
+    for (let i = 0; i < array2.length; i++) {
+        newArray.push(array2[i])
+    }
+    return newArray
+}
+
